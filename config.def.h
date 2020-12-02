08593d0e87d13e9baddaafa406363ce6d26ec6c8
Cornel <cornel.paduraru87@gmail.com>
2020-12-02 16:34:46 +0200

    Initial build.

---
+++ config.def.h
@@ -0,0 +1,24 @@
+/* user and group to drop privileges to */
+static const char *user  = "cornel";
+static const char *group = "cornel";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius = 5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize = 0;
